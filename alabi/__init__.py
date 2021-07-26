cd4eec031b2bd8e52f5ce915b6a1dc61b76addf1
Jessica Birky <jbirky@uw.edu>
2021-07-25 21:35:21 -0700

    import benchmarks

--- alabi/__init__.py
+++ alabi/__init__.py
@@ -1,6 +1,11 @@
+# __version__ = "0.1.0"
+__author__ = "Jessica Birky (jbirky@uw.edu)"
+__copyright__ = "Copyright 2021 Jessica Birky"
+
 from .core import *
 from .utility import *
 from .gp_utils import *
 from .mcmc_utils import *
 from .cache_utils import *
 from .visualization import *
+from .benchmarks import *
